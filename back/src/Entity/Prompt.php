0c9994890da639e0611c216496e4ab0e4c407987
maxdlr <contact@maxdlr.com>
2024-02-08 09:25:24 +0100

    add Prompt Entity

---
+++ back/src/Entity/Prompt.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PromptRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PromptRepository::class)]
+#[ApiResource]
+class Prompt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $day = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nameFr = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nameEn = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?int
+    {
+        return $this->day;
+    }
+
+    public function setDay(int $day): static
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getNameFr(): ?string
+    {
+        return $this->nameFr;
+    }
+
+    public function setNameFr(string $nameFr): static
+    {
+        $this->nameFr = $nameFr;
+
+        return $this;
+    }
+
+    public function getNameEn(): ?string
+    {
+        return $this->nameEn;
+    }
+
+    public function setNameEn(string $nameEn): static
+    {
+        $this->nameEn = $nameEn;
+
+        return $this;
+    }
+}
