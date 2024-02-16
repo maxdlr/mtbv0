2da76dddc3d133e1e5c4f5201d6298075053a880
maxdlr <contact@maxdlr.com>
2024-02-16 17:32:38 +0100

    prepare deployment

--- front/apiConfig/config.js
+++ front/apiConfig/config.js
@@ -1,3 +1,3 @@
-export const localHost = '127.0.0.1'
+export const apiDomain = '127.0.0.1'
 export const apiPort = '8000'
-export const BASE_API_URL = `https://${localHost}:${apiPort}/api`
+export const BASE_API_URL = `https://${apiDomain}:${apiPort}/api`
